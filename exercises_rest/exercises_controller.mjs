65500a83c440418ccf52941556afbc0f095bd10f
Sam Bassong <sam.bassong@gmail.com>
2025-05-28 15:49:07 -0700

    starter set up

---
+++ exercises_rest/exercises_controller.mjs
@@ -0,0 +1,17 @@
+/**
+ * Add your first name and last name.
+ */
+import 'dotenv/config';
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import * as exercises from './exercises_model.mjs';
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+
+app.listen(PORT, async () => {
+    await exercises.connect()
+    console.log(`Server listening on port ${PORT}...`);
+});
---
+++ exercises_rest/exercises_model.mjs
@@ -0,0 +1,26 @@
+/**
+ * Add your first name and last name.
+ */
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const EXERCISE_DB_NAME = 'exercise_db';
+
+let connection = undefined;
+
+/**
+ * This function connects to the MongoDB server and to the database
+ *  'exercise_db' in that server.
+ */
+async function connect(){
+    try{
+        connection = await mongoose.connect(process.env.MONGODB_CONNECT_STRING, 
+                {dbName: EXERCISE_DB_NAME});
+        console.log("Successfully connected to MongoDB using Mongoose!");
+    } catch(err){
+        console.log(err);
+        throw Error(`Could not connect to MongoDB ${err.message}`)
+    }
+}
+
+export { connect};
